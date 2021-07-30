9887fc725b6e79c9844eb50a541daa9f79695ebd
Attila Fulop <1162360+fulopattila122@users.noreply.github.com>
2021-07-30 10:43:43 +0200

    Added generic gateway tests

---
+++ tests/Gateway/RegistrationTest.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Vanilo\Adyen\Tests\Gateway;
+
+use Vanilo\Payment\Contracts\PaymentGateway;
+use Vanilo\Payment\PaymentGateways;
+use Vanilo\Adyen\AdyenPaymentGateway;
+use Vanilo\Adyen\Tests\TestCase;
+
+class RegistrationTest extends TestCase
+{
+    /** @test */
+    public function the_gateway_is_registered_out_of_the_box_with_defaults()
+    {
+        $this->assertCount(2, PaymentGateways::ids());
+        $this->assertContains(AdyenPaymentGateway::DEFAULT_ID, PaymentGateways::ids());
+    }
+
+    /** @test */
+    public function the_gateway_can_be_instantiated()
+    {
+        $gateway = PaymentGateways::make('adyen');
+
+        $this->assertInstanceOf(PaymentGateway::class, $gateway);
+        $this->assertInstanceOf(AdyenPaymentGateway::class, $gateway);
+    }
+}
